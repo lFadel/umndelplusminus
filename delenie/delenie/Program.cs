051382a253c89e2d3bbd7b69a2ed2455aae28081
Fade <galimov-stepan@bk.ru>
2024-09-13 13:44:33 +0400

    Add files via upload

---
+++ delenie/delenie/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите первое значение:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе значение:");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(a / b);
