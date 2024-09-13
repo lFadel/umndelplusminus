3b67a7c071c70f0c1f8f8556633ca9cc86908ad5
Fade <galimov-stepan@bk.ru>
2024-09-13 13:39:51 +0400

    Delete Minus directory

--- Minus/Minus/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿Console.WriteLine("Введите первое значение:");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите второе значение:");
-int b = int.Parse(Console.ReadLine());
-Console.WriteLine(a - b);
